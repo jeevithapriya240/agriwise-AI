1bb3291656af9f602130ff454f6eac9f0dfdb1fe
kunal-js-dev <kunalkunal2915@gmail.com>
2026-09-15 19:25:38 +0530

    Initial commit

---
+++ #selecitive statement.py
@@ -0,0 +1,10 @@
+color=input("enter the chocholate  :")
+match color :
+    case "munch":
+        print("😎")
+    case "dairymilk":
+        print("😋")
+    case "kitkat":
+        print("😊")
+    case default:
+        print("invalid chocholate")
